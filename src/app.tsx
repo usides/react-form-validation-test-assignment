bc4eb2e8acef50dc28f3281ae55e3c47a8b1a2ac
Aleksei Efimov <aleksei.efimov@mercurydevelopment.com>
2023-02-08 21:53:10 +0400

    chore: initial commit

---
+++ src/app.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import st from "./app.module.scss";
+
+export const App: React.FC = () => {
+  return <div className={st["app"]}></div>;
+};
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "src/assets/styles/index.scss";
+import { App } from "./app";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
